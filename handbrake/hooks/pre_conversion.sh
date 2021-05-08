d1a04e140a4723a46689249b5fb5ea186db91d0b
Matthew meade <mafumeade@gmail.com>
2021-05-07 22:12:18 -0230

    Add handbrake to automatically compress recordings

---
+++ handbrake/hooks/pre_conversion.sh
@@ -0,0 +1,3 @@
+# Copy the input video to the output folder so it is available while the video is being encoded
+# Will be overwritten by the encoded video when it is complete
+cp "$SOURCE_FILE" "$CONVERTED_FILE" 
